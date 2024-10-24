cc6f1b5f8d9e504c0f7af78749e791daa7189b37
owen22s <34884@ma-web.nl>
2024-10-24 11:59:19 +0100

    added a visual indicator for enemy attacks

--- Strategy_Pattern/Assets/Scripts/enemy/EnemyAttack.cs
+++ Strategy_Pattern/Assets/Scripts/enemy/EnemyAttack.cs
@@ -1,26 +1,56 @@
+using TMPro;
 using UnityEngine;
 
 public class EnemyAttack : MonoBehaviour
 {
-    public PlayerHealth playerHealth;
-
+    [SerializeField] private TextMeshProUGUI enemyDamageText; // Display the damage dealt
+    [SerializeField] private TextMeshProUGUI damagePreviewText; // Display the damage preview
+    [SerializeField] private PlayerHealth playerHealth;
+    private int currentDamage;
     public int[] Attacks = { 7, 11, 36 };
 
     void Start()
     {
+
         TurnSystem.TurnEnded.AddListener(Attack);
-    }
-
-
-    void Update()
-    {
-
+        GenerateRandomAttack();
+        ShowDamagePreview(currentDamage);
     }
 
     public void Attack()
     {
-        int randomAttack = Random.Range(0, Attacks.Length);
-        int damage = Attacks[randomAttack];
+        Invoke(nameof(ApplyDamage), 1f);
+        GenerateRandomAttack();
+        ShowDamagePreview(currentDamage);
+    }
+    public void GenerateRandomAttack()
+    {
+        int randomAttackIndex = Random.Range(0, Attacks.Length);
+        currentDamage = Attacks[randomAttackIndex];
+        Debug.Log("generated Random attack");
+    }
+
+    private void ShowDamagePreview(int damage)
+    {
+        damagePreviewText.text = $"Enemy will deal {damage} damage!";
+        damagePreviewText.gameObject.SetActive(true);
+    }
+
+    private void ApplyDamage(int damage)
+    {
         playerHealth.health -= damage;
+
+        if (playerHealth.health < 0)
+        {
+            playerHealth.health = 0;
+        }
+    }
+    private void ApplyDamage()
+    {
+        playerHealth.health -= currentDamage;
+        if (playerHealth.health < 0)
+        {
+            playerHealth.health = 0;
+        }
     }
 }
